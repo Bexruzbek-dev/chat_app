d9cce4077b6b34202b057a985c9e92aa1a8a929a
Bexruzbek-dev <behruz.toshpolatov14@gmail.com>
2024-07-06 11:51:58 +0500

    reunf

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app_with_mk/services/auth/auth_gate.dart';
+import 'package:chat_app_with_mk/firebase_options.dart';
+import 'package:chat_app_with_mk/services/chat/push_notifications_service.dart';
+import 'package:chat_app_with_mk/theme/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebasePushNotificationService.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const AuthGate(),
+      theme: lightMode,
+    );
+  }
+}
---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,54 @@
+import 'package:chat_app_with_mk/services/chat/push_notifications_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
+  Future<UserCredential> singinWithEmailAndPassword(
+      String email, password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  Future<UserCredential> signUpWithEmailAndPassword(
+      String email, password) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      String? token = await FirebasePushNotificationService.getToken();
+
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'token': token,
+          'uid': userCredential.user!.uid,
+          'email': email,
+        },
+      );
+
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
+}
